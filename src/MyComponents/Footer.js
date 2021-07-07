669678db141732fb52b13a95ba367694eb186190
Aakash tyagi <akashtyagi3990@gmail.com>
2021-07-07 13:17:25 +0530

    message

---
+++ src/MyComponents/Footer.js
@@ -0,0 +1,20 @@
+import React from 'react'
+// import './footer.css'
+
+export const Footer = () => {
+    // let footerStyle = {
+    //     position:"relative",
+    //     top:"1vh",
+    //     width:"100%"
+    // }
+
+    return (
+        <footer className = "bg-dark text-light py-3 ">
+            <p className="text-center">
+                copyright &copy; MyTodosList.com
+            </p>
+        
+            
+        </footer>
+    )
+}
